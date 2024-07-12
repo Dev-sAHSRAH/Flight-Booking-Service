656ed406f4f50d48993e6cc903f9c62b6a8ff97f
harsha <c.harsha0909@gmail.com>
2024-07-12 15:15:41 +0530

    Added booking model and utils

---
+++ src/models/booking.js
@@ -0,0 +1,47 @@
+"use strict";
+const { Model } = require("sequelize");
+const { BOOKING_STATUS } = require("../utils/common/enums");
+const { BOOKED, CANCELLED, INITIATED, PENDING } = BOOKING_STATUS;
+module.exports = (sequelize, DataTypes) => {
+  class Booking extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Booking.init(
+    {
+      flightId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.ENUM,
+        values: [BOOKED, CANCELLED, INITIATED, PENDING],
+        defaultValue: INITIATED,
+        allowNull: false,
+      },
+      noOfSeats: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      totalCost: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Booking",
+    }
+  );
+  return Booking;
+};
---
+++ src/utils/common/enums.js
@@ -0,0 +1,15 @@
+const SEAT_TYPE = {
+  BUSINESS: "business",
+  ECONOMY: "economy",
+  PREMIUM_ECONOMY: "premium_economy",
+  FIRST_CLASS: "first-class",
+};
+
+const BOOKING_STATUS = {
+  BOOKED: "booked",
+  CANCELLED: "cancelled",
+  INITIATED: "initiated",
+  PENDING: "pending",
+};
+
+module.exports = { SEAT_TYPE, BOOKING_STATUS };
