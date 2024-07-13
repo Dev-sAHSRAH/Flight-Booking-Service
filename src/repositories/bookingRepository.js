03e2fbaaa3b3b6fe06518611c4175395a55a33f3
harsha <c.harsha0909@gmail.com>
2024-07-13 10:47:01 +0530

    Added MVC

---
+++ src/repositories/bookingRepository.js
@@ -0,0 +1,12 @@
+const { StatusCode } = require("http-status-codes");
+
+const { Booking } = require("../models");
+const CrudRepository = require("./crud-repository");
+
+class BookingRepository extends CrudRepository {
+  constructor() {
+    super(Booking);
+  }
+}
+
+module.exports = BookingRepository;
